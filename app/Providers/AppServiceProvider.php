c4751ad413476170f4d91c3938caef271bb19ad7
Moise BISHOBOKERUWIZEYE <bishomoise84@gmail.com>
2022-11-01 21:09:12 -0800

    Authentication and create channel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if(App::environment() === "production")
         {
-            URL::forceScheme("http");
+            URL::forceScheme("https");
         }
     }
 }
