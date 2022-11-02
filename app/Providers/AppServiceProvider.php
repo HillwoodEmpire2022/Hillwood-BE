cb952548c24b2dd342ec55d9dc51784c83b2715a
Moise BISHOBOKERUWIZEYE <bishomoise84@gmail.com>
2022-11-01 16:09:31 -0800

    Authentication and create channel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\URL;
+// use Illuminate\Support\Facades\App;
+// use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,9 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(App::environment() === "production")
-        {
-            URL::forceScheme("https");
-        }
+        // if(App::environment() === "production")
+        // {
+        //     URL::forceScheme("https");
+        // }
     }
 }
